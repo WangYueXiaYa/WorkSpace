b39f50db0c8727de9ce601459b86438a6a22b9a3
望月夏芽 <1827013497@qq.com>
2020-06-19 17:18:50 +0800

    two changes

--- Demo.java
+++ Demo.java
@@ -3,5 +3,6 @@ public class Demo{
 		System.out.println("I love javascript");
 		System.out.println("I am a little girl");
 		System.out.println("I love html and css");
+		System.out.println("This is a little change");
 	}
 }
